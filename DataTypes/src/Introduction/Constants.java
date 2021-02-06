25403d45c2ac8766bfa77441e2f5a6af474a492f
Rodrigo <cuitlagua@gmail.com>
2021-02-05 23:15:52 -0600

    Constants

---
+++ DataTypes/src/Introduction/Constants.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Introduction;
+
+/**
+ *
+ * @author development
+ */
+public class Constants {
+    public static void constants(String[] args) {
+    
+    final int numero = 10; // con el prefijo final indicamos la constante
+
+    System.out.println(numero);
+
+    }
+       
+}
