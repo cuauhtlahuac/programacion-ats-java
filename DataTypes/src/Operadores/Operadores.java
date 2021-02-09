6e8532212bcaed6c72af3cbd8179af7723dea0df
Rodrigo <cuitlagua@gmail.com>
2021-02-08 22:59:26 -0600

    operadores

---
+++ DataTypes/src/Operadores/Operadores.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Operadores;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author development
+ */
+public class Operadores {
+    public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
+        float numero1, numero2, suma, resta, mult, div, residuo;
+        
+        System.out.println("Digita dos numeros");
+        numero1 = entrada.nextFloat();
+        numero2 = entrada.nextFloat();
+        
+        suma = numero1 + numero2;
+        resta = numero1 - numero2;
+        mult = numero1 * numero2;
+        div = numero1 / numero2;
+        residuo = numero1 % numero2;
+        
+        System.out.println("suma = " + suma);
+        System.out.println("resta = " + resta);
+        System.out.println("mult = " + mult);
+        System.out.println("div = " + div);
+        System.out.println("residuo = " + residuo);
+        
+    }
+}
