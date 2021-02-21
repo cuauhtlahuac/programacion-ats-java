a2a7ff33d4a1a560d3db281b45cada55e5632671
Rodrigo <cuitlagua@gmail.com>
2021-02-20 23:49:38 -0600

    profesor solution

--- DataTypes/src/Condicionales/Ordenar3Numeros.java
+++ DataTypes/src/Condicionales/Ordenar3Numeros.java
@@ -12,58 +12,57 @@ import javax.swing.JOptionPane;
  * @author development
  */
 public class Ordenar3Numeros {
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         int n1, n2, n3;
-        
+
         int temp = 0;
-        
-        n1 = Integer.parseInt(JOptionPane.showInputDialog("Digita un número"));      
+
+        n1 = Integer.parseInt(JOptionPane.showInputDialog("Digita un número"));
         n2 = Integer.parseInt(JOptionPane.showInputDialog("Digita un número diferente a: " + n1));
         n3 = Integer.parseInt(JOptionPane.showInputDialog("Digita un número diferente a: " + n2 + "  y  " + n1));
-        
+
         // n1 = 3; n2 = 9; n3 = 6;
         // n1 = 6; n2 = 3; n3 = 9;
         // n1 = 9; n2 = 6; n3 = 3;
-        
         // n1 = 9; n2 = 6; n3 = 3;
         // n1 = 3; n2 = 9; n3 = 6;
-        // n1 = 6; n2 = 3; n3 = 9; *
-        
+        // n1 = 6; n2 = 3; n3 = 9;
         // n1 = 6; n2 = 3; n3 = 9;
         // n1 = 9; n2 = 6; n3 = 3;
         // n1 = 3; n2 = 9; n3 = 6;
-        
-        if(n1 > n3){ // 6 9
 
-        }else{
-          temp = n1; // 6
-          n1 = n3; // 9
-          n3 = temp; // 6
-        }
-        // n1 = 9; n3 = 6; n2 = 3;
-        
-        if(n1 > n2){ // 9 3
+           // Caso n1 Mayor
 
-        }else{
-          temp = n1;
-          n1 = n2;
-          n2 = temp;
-        }
-        // n1 = 9; n3 = 6; n2 = 3;
+        if (n1 > n2 && n2 > n3) {
+            JOptionPane.showMessageDialog(null,
+                    "Orden: " + n1 + " - " + n2 + " - " + n3);
+        } else if (n1 > n3 && n3 > n2) {
+            JOptionPane.showMessageDialog(null,
+                    "Orden: " + n1 + " - " + n3 + " - " + n2);
 
-        if(n2 > n3){ // 3 6
-          
-        }else{
-          temp = n2; // 3
-          n2 = n3; // 6
-          n3 = temp; // 3
+            // Caso n2 Mayor   
+
+        } else if (n2 > n1 && n1 > n3) {
+            JOptionPane.showMessageDialog(null,
+                    "Orden: " + n2 + " - " + n1 + " - " + n3);
+        } else if (n2 > n3 && n3 > n1) {
+            JOptionPane.showMessageDialog(null,
+                    "Orden: " + n2 + " - " + n3 + " - " + n1);
+
+            // Caso n3 Mayor    
+
+        } else if (n3 > n1 && n1 > n2) {
+            JOptionPane.showMessageDialog(null,
+                    "Orden: " + n3 + " - " + n1 + " - " + n2);
+        } else if (n3 > n2 && n2 > n1) {
+            JOptionPane.showMessageDialog(null,
+                    "Orden: " + n3 + " - " + n2 + " - " + n1);
         }
-        
-        // n1 = 9; n2 = 6; n3 = 3; 
-       
+
         JOptionPane.showMessageDialog(null,
                 "You Write: " + n1 + "," + n2 + "," + n3 + "\n");
-        
+
     }
-   
+
 }
