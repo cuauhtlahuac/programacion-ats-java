91a0768dc27cc80a0599c6b8bfc6d49866359f8e
Rodrigo <cuitlagua@gmail.com>
2021-02-21 20:34:25 -0600

    Creating a Car Class and calling his first method

---
+++ HackerRank/src/hackerrank/Car.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hackerrank;
+
+/**
+ *
+ * @author development
+ */
+public class Car {
+    
+    int maxSpeed = 100;
+    int minSpeed = 0;
+    double weight = 4079;
+    boolean isTheCarOn = false;
+    char condition = 'A';
+    
+    public void printVariables(){ // Public is the scope of the funcion, viod = return nothing
+        System.out.println(maxSpeed);
+        System.out.println(minSpeed);
+        System.out.println(weight);
+        System.out.println(isTheCarOn);
+        System.out.println(condition);
+    }
+    
+    
+    public static void main(String[] args){
+        System.out.println("I gonna print in the HackerRank class, but this print won't be show out.");
+        
+    }
+    
+}
---
+++ HackerRank/src/hackerrank/HackerRank.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hackerrank;
+
+/**
+ *
+ * @author development
+ */
+public class HackerRank {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // We gonna create a instance of a car in order to called
+        
+        Car myCar = new Car();
+        myCar.printVariables();
+    }
+    
+}
