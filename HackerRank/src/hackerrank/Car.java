bfe555efa70b120348598a450408d1e93a1656a4
Rodrigo <cuitlagua@gmail.com>
2021-03-06 22:24:21 -0600

    fix and print evaluations

--- HackerRank/src/hackerrank/Car.java
+++ HackerRank/src/hackerrank/Car.java
@@ -10,33 +10,32 @@ package hackerrank;
  * @author development
  */
 public class Car {
-    
+
     int maxSpeed = 100;
     int minSpeed = 0;
     double weight = 4079;
     boolean isTheCarOn = false;
     char condition = 'A';
     String nameOfCar = "Lucy"; // Double quotes in Referenced Type String
-      
+
     double maxFuel = 16;
     double currentFuel = 8;
     double mpg = 26.5; // mileages per gallon
-    
+
     int numberOfPeopleInCar = 1;
     int maxCarCapacity = 4;
-    
-    
+
     //    Constucture ▼   
     public Car(int customMaxSpeed, double customWeight, boolean customIsTheCarOn, String customCarName, int customeMaxCarCapacity) {
-        maxSpeed  = customMaxSpeed;
+        maxSpeed = customMaxSpeed;
         weight = customWeight;
         isTheCarOn = customIsTheCarOn;
         nameOfCar = customCarName;
         maxCarCapacity = customeMaxCarCapacity;
     }
     //    Constucture ▲
-    
-    public void printVariables(){ // Public is the scope of the funcion, viod = return nothing
+
+    public void printVariables() { // Public is the scope of the funcion, viod = return nothing
         System.out.println("Max Speed:" + maxSpeed);
         System.out.println("min Speed: " + minSpeed);
         System.out.println("Weight: " + weight);
@@ -46,40 +45,45 @@ public class Car {
         System.out.println("Number of people in the car:  " + numberOfPeopleInCar);
         System.out.println("\n");
     }
-    
-    public void upgradeSpeed(){
+
+    public void upgradeSpeed() {
         minSpeed = maxSpeed;
         maxSpeed += 1;
     }
-    
+
     public void getIn() {
-        if(numberOfPeopleInCar < maxCarCapacity)
-        numberOfPeopleInCar++;
-    }
-    
-    public void getOut() {
-        if(numberOfPeopleInCar > 1){
-            numberOfPeopleInCar--;
+        if (numberOfPeopleInCar < maxCarCapacity) {
+            numberOfPeopleInCar++;
+            System.out.println("Someone got in");
+        } else {
+            System.out.println("The Car is full! " + numberOfPeopleInCar + " = " + maxCarCapacity);
         }
     }
-    
+
+    public void getOut() {
+        if (numberOfPeopleInCar > 0) {
+            numberOfPeopleInCar--;
+            System.out.println("Someone left the car");
+        }
+    }
+
     public double howManyMillesTillOutOfgas() {
         return currentFuel * mpg;
     }
-    
-    public double maxMilesPerFillUp () {
+
+    public double maxMilesPerFillUp() {
         return maxFuel * mpg;
     }
-    
+
     public void turnoTheCarOn() {
-        if(!isTheCarOn) {
+        if (!isTheCarOn) {
             isTheCarOn = true;
         }
     }
-    
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         System.out.println("I gonna print in the HackerRank class, but this print won't be show out.");
-        
+
     }
-    
+
 }
